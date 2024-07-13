8cecd978ca28702300addb87aff05780989a30e2
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-07-13 06:11:41 -0700

    asd

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# This Puppet Create The School File
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+  path    => '/tmp/school',
+}
+  
